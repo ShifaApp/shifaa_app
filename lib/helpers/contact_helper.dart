8dd5e5fabbacc631aec1b1b39fb2406d1a03bb29
ManalM <mmsaj000@hotmail.com>
2022-11-05 16:05:04 +0300

    last version done

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -3,42 +3,6 @@ import 'dart:io';
 import 'package:url_launcher/url_launcher.dart';
 
 
-void openPDF(String path)async{
-  String filePath = path;
-  final Uri uri = Uri.file(filePath);
-
-  if (await File(uri.toFilePath()).exists()) {
-    if (!await launch(uri.toString())) {
-      throw 'Could not launch $uri';
-    }
-  }
-}
-
-void openTwitterApp(String address)async{
-
-  String url = "twitter://user?screen_name=";
-
-  if (await canLaunch(url+address)) {
-    await launch(url+address);
-  } else {
-    url = "https://twitter.com/";
-    if (await canLaunch(url+address)) {
-      await launch(url+address);
-    }else{
-      throw 'Could not launch twitter';
-    }
-
-  }
-}
-//--------------
-void openURL(String url) async {
-
-  if (await canLaunch(url)) {
-    await launch(url);
-  } else {
-    throw 'Could not launch $url';
-  }
-}
 //--------------
 void directToPhoneCall(String number) async {
 
@@ -57,47 +21,3 @@ void directToPhoneCall(String number) async {
 
 //--------------
 
-void openMap(num lat, num lng) async {
-  // Android
-  var url = 'geo:$lat,$lng';
-  if (Platform.isIOS) {
-    // iOS
-    url = 'http://maps.apple.com/?ll=$lat,$lng';
-  }
-  if (await canLaunch(url)) {
-    await launch(url);
-  } else {
-    throw 'Could not launch $url';
-  }
-}
-
-//--------------
-void sendMail(String email) async {
-  // Android and iOS
-  var uri = 'mailto:$email';
-  if (await canLaunch(uri)) {
-    await launch(uri);
-  } else {
-    throw 'Could not launch $uri';
-  }
-}
-
-void openWhatsApp(String phone)async{
-
-  //add twitter and whatsapp
-  String message = 'hello ';
-  String uri;
-  if (Platform.isAndroid) {
-    // add the [https]
-    uri= "https://wa.me/$phone/?text=${Uri.parse(message)}"; // new line
-  } else {
-    // add the [https]
-    uri= "https://api.whatsapp.com/send?phone=$phone";   //=${Uri.parse(message)}"; // new line
-  }
-//  String uri= 'https://api.whatsapp.com/send?phone=$phone';
-  if (await canLaunch(uri)) {
-    await launch(uri);
-  } else {
-    throw 'Could not launch $uri';
-  }
-}
